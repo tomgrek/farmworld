358d2e591829e7df50e7127622d50c68b15723f7
tomgrek <tom.grek@gmail.com>
2022-11-22 20:15:43 -0800

    still works

--- farmworld/geojson/util/__init__.py
+++ farmworld/geojson/util/__init__.py
@@ -2,7 +2,7 @@ import math
 
 from farmworld.const import SKY
 
-def get_covered_area(bitmap, screen_size):
+def get_covered_area(bitmap, screen_size, grid_size):
     """Return fraction of area (screen_size) that's fields.
     Assume 32-bit RGBA"""
     buf = bitmap.get_buffer().raw
@@ -11,4 +11,4 @@ def get_covered_area(bitmap, screen_size):
         b, g, r, a = buf[px : px + 4]
         if (r, g, b) == SKY:
             bgcol += 1
-    return (math.prod(screen_size) - bgcol) / math.prod(screen_size)
+    return ((math.prod(screen_size) - bgcol) / math.prod(screen_size)) * (grid_size**2)
--- farmworld/renderer/__init__.py
+++ farmworld/renderer/__init__.py
@@ -7,7 +7,8 @@ class Renderer:
     def __init__(self, screen_size):
         pygame.init()
         self.font = pygame.font.SysFont("dejavusans", 16)
-        self.screen = pygame.display.set_mode(screen_size)
+        self.screen_size = screen_size
+        self.screen = pygame.display.set_mode(self.screen_size)
     
     def quit(self):
         if self.screen is not None:
@@ -22,3 +23,10 @@ class Renderer:
         for i, text in enumerate(info):
             foreground = self.font.render(text, True, (0, 0, 0))
             bitmap.blit(foreground, (0, i * 16))
+    
+    def get_surface(self, color, transparency=False):
+        surface = pygame.Surface(self.screen_size, pygame.SRCALPHA)
+        if transparency:
+            surface.convert_alpha()
+        surface.fill(color)
+        return surface
