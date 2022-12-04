ce0c8b82e56fc4943d852402d0b34c1deb0ccf93
tomgrek <tom.grek@gmail.com>
2022-12-03 19:19:08 -0800

    working resume training

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 import argparse
 
-import torch
-
 from stable_baselines3 import PPO, DQN
 from sb3_contrib import RecurrentPPO, TRPO
 from stable_baselines3.common.vec_env.dummy_vec_env import DummyVecEnv
@@ -14,6 +12,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--no-resume", action="store_true", help="Continue training from modelpolicy.bin")
 parser.add_argument("--no-save", action="store_true", default=True, help="Save policy to modelpolicy.bin")
 parser.add_argument("--steps", type=int, default=50000, help="Number of timesteps to train")
+parser.add_argument("--eps", type=float, default=0.4, help="Initial exploration")
 parser.add_argument("--filename", type=str, default="modelpolicy.bin", help="Name of file to save policy to.")
 parser.set_defaults(no_resume=False, no_save=False)
 
@@ -22,18 +21,28 @@ if __name__ == "__main__":
     print(f"Running with following CLI options: {args}")
     GAMMA = 0.995
 
-    env = farmworld.env.FarmEnv(screen_size=(700, 700), num_fields=4, max_fps=40) # geojson="example2.json", 
-    env = VecNormalize(DummyVecEnv([lambda: env]), gamma=GAMMA, clip_reward=1000, clip_obs=1000)
+    # env = farmworld.env.FarmEnv(screen_size=(700, 700), num_fields=4, max_fps=40) # geojson="example2.json", 
+    # env = VecNormalize(DummyVecEnv([lambda: env]), gamma=GAMMA, clip_reward=1000, clip_obs=1000)
     set_random_seed(42)
 
     #model = PPO("MultiInputPolicy", env, verbose=1, gamma=0.999, ent_coef=0.1)#0.001)
     #model = TRPO("MultiInputPolicy", env, verbose=1)
-    #model = RecurrentPPO("MultiInputLstmPolicy", env, verbose=1, gamma=0.95, ent_coef=0.0, learning_rate=0.003)#0.1)
-    model = DQN("MultiInputPolicy", env, learning_starts=10000, gamma=GAMMA, exploration_fraction=0.5, exploration_initial_eps=0.4, verbose=1)
+    #model = RecurrentPPO("MultiInputLstmPolicy", env, verbose=1, gamma=0.95, ent_coef=0.0, learning_rate=0.003)#0.1)    
 
     if not args.no_resume:
-        print(f"Loading policy from {args.filename}")
-        model.policy = torch.load(args.filename)
+        env = farmworld.env.FarmEnv(screen_size=(700, 700), num_fields=4, max_fps=40)
+        env = DummyVecEnv([lambda: env])
+        env = VecNormalize.load(args.filename + ".env", env)
+        print(f"Loaded vec env from {args.filename + '.env'}")
+    else:
+        env = farmworld.env.FarmEnv(screen_size=(700, 700), num_fields=4, max_fps=40) # geojson="example2.json", 
+        env = VecNormalize(DummyVecEnv([lambda: env]), gamma=GAMMA, clip_reward=1000, clip_obs=1000)
+    
+    model = DQN("MultiInputPolicy", env, learning_starts=10000, gamma=GAMMA, exploration_fraction=0.5, exploration_initial_eps=args.eps, verbose=1)
+    if not args.no_resume:
+        print(f"Loaded policy from {args.filename}")
+        model.set_parameters(args.filename)
+        model.set_env(env)
     
     def logger(accum, other):
         for info in accum.get("infos", []):
@@ -43,13 +52,16 @@ if __name__ == "__main__":
                     print(f"{k}: {v}")
                 print("-------")
 
-    model.learn(total_timesteps=args.steps, callback=logger)
+    if args.steps > 0:
+        model.learn(total_timesteps=args.steps, callback=logger)
     if not args.no_save:
-        print(f"Saved policy to {args.filename}")
-        torch.save(model.policy, args.filename)
-
+        model.save(args.filename)
+        model.get_env().save(args.filename + '.env')
+        print(f"Saved policy to {args.filename} and vec env to {args.filename + '.env'}")
+    
     # Demo the policy, with rendering
     env.training = False
+    env.norm_reward = False
     obs = env.reset()
     max_its = 20000000
     its = 0
